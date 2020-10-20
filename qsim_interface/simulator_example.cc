86710e9b849f22a59025483a810736e738470d36
dkafri <dkafri@google.com>
2020-10-20 11:43:30 -0700

    update example

--- qsim_interface/simulator_example.cc
+++ qsim_interface/simulator_example.cc
@@ -8,6 +8,7 @@
 #include <formux.h>
 #include <gate_appl.h>
 #include <iostream>
+#include <complex>
 
 using namespace std;
 using Simulator = qsim::Simulator<qsim::For>;
@@ -24,7 +25,7 @@ void print_amplitudes(State& state) {
 }
 
 int main() {
-
+  using Complex = complex<fp_type>;
   unsigned num_threads = 4;
   unsigned num_qubits = 2;
   Simulator simulator(num_threads);
@@ -39,17 +40,22 @@ int main() {
   qsim::ApplyGate(simulator, X, state);
   print_amplitudes(state);
 
-  //Apply CNOT 0->1, expect |11> but still get |10>
-  qsim::Cirq::Matrix2q<Simulator::fp_type> CNOT_mat{1, 0, 0, 0,
-                                                    0, 0, 0, 1,
-                                                    0, 0, 1, 0,
-                                                    0, 1, 0, 0};
-  auto CNOT = qsim::Cirq::MatrixGate2<fp_type>::Create(0,
-                                                       0, 1,
-                                                       CNOT_mat);
-  qsim::ApplyGate(simulator, CNOT,state);
-  print_amplitudes(state);
+  array<Complex, 4> row0{1, 0, 0, 0};
+  array<Complex, 4> row1{0, 0, 0, 1};
+  array<Complex, 4> row2{0, 0, 1, 0};
+  array<Complex, 4> row3{0, 1, 0, 0};
 
+  //Apply CNOT 0->1, expect |11> but still get |10>
+  qsim::Cirq::Matrix2q<Simulator::fp_type> CNOT_mat{row0, row1, row2, row3};
+
+  auto CNOT = qsim::Cirq::MatrixGate2<fp_type>::Create(0, 0, 1,
+                                                       CNOT_mat);
+
+  // Using the CNOT gate directly works
+  //  auto CNOT = qsim::Cirq::CNOT<fp_type>::Create(0,0,1);
+
+  qsim::ApplyGate(simulator, CNOT, state);
+  print_amplitudes(state);
 
   return 0;
 }
