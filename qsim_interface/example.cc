0531668f1e41277711cb96aeccba716e32d2953a
dkafri <dkafri@google.com>
2020-10-28 16:38:17 -0700

    flesh out example

--- qsim_interface/example.cc
+++ qsim_interface/example.cc
@@ -4,12 +4,37 @@
 
 #include <pybind11/pybind11.h>
 
+#include <utility>
+
+namespace py = pybind11;
+
 int add(int i, int j) {
-    return i + j +1;
+  return i + j;
 }
 
-PYBIND11_MODULE(pybind_example, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
+struct Pet {
+  explicit Pet(std::string name) : name(std::move(name)), age(0) {}
+  void setName(const std::string& name_) { name = name_; }
+  const std::string& getName() const { return name; }
 
-    m.def("add", &add, "A function which adds two numbers");
+  std::string name;
+  int age;
+};
+
+PYBIND11_MODULE(pybind_example, m) {
+  m.doc() = "pybind11 example plugin"; // optional module docstring
+
+  m.def("add", &add, "A function which adds two numbers",
+        py::arg("i") = 1, py::arg("j") = 2);
+
+  py::class_<Pet>(m, "Pet")
+      .def(py::init<std::string>()) //bind constructor
+      .def("setName", &Pet::setName) //bind method
+      .def("getName", &Pet::getName)
+      .def("__repr__", // bind lambda function
+           [](const Pet& a) {
+             return "<example.Pet named '" + a.name + "'>";
+           })
+      .def_readwrite("age", &Pet::age); // give attribute access
+
 }
