c5aebc89127d9af97aec6f19b0d08a926633d299
dkafri <dkafri@google.com>
2020-11-04 20:40:41 -0800

    sampling method that just returns register values

--- qsim_interface/test_interface.py
+++ qsim_interface/test_interface.py
@@ -68,3 +68,60 @@ def test_add_cop():
                   }
 
   sampler_cpp.add_coperation(channels_map, ("c",), {"b"}, False)
+
+
+def test_samples():
+  max_qubits = 4
+  sampler_cpp = pbi.Sampler(3, max_qubits)
+  sampler_cpp.set_random_seed(11)
+  cond_reg = "R"
+  sampler_cpp.set_initial_registers({cond_reg: 0})
+  labels = [cond_reg]
+
+  # state_vec = np.arange((2 ** max_qubits)).astype(ComplexType)
+  state_vec = np.zeros((2 ** max_qubits), ComplexType)
+  state_vec[0] = 1.0
+  axes = ["a", "b", "c"]
+  sampler_cpp.bind_initial_state(state_vec, axes)
+
+  sqrt_half = np.sqrt(0.5)
+  channels = {
+      (0,): [
+          [sqrt_half * np.eye(2, dtype=ComplexType).ravel(), [], ["a"], [], [],
+           []],
+          [sqrt_half * np.array([0, 1, 1, 0], ComplexType), [], ["a"], [], [],
+           []]
+      ]}
+  cond_regs = (cond_reg,)
+  is_recorded = True
+  is_virtual = False
+  label = "flip_a"
+  labels.append(label)
+
+  sampler_cpp.add_koperation(channels, cond_regs, is_recorded, label,
+                             is_virtual)
+
+  channels = {
+      (0,): [
+          [np.array([1, 0, 0, 0], ComplexType), [], ["a"], [], [], ["a"]],
+          [np.array([0, 1, 0, 0], ComplexType), [], ["a"], [], [], ["a"]]
+      ]}
+  cond_regs = (cond_reg,)
+  is_recorded = True
+  is_virtual = False
+  label = "measure_a"
+  labels.append(label)
+
+  sampler_cpp.add_koperation(channels, cond_regs, is_recorded, label,
+                             is_virtual)
+
+  sampler_cpp.set_register_order(labels)
+
+  reg_mat_cpp = sampler_cpp.sample_states(10)
+  reg_mat = np.array(reg_mat_cpp, copy=False)
+
+  assert reg_mat.shape == (10, 3)
+  # expect second and third columns to always agree, first column to always
+  # be zero
+  assert np.array_equal(reg_mat[:, 0], np.zeros_like(reg_mat[:, 0]))
+  assert np.array_equal(reg_mat[:, 1], reg_mat[:, 2])
