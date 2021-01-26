091635160c30dc9aa52a96d7a5f1d92788d18e35
dkafri <dkafri@google.com>
2021-01-25 16:50:43 -0800

    comments, fusion plan

--- qsim_interface/include/state_rep.h
+++ qsim_interface/include/state_rep.h
@@ -366,7 +366,8 @@ class KState {
   void permute_and_apply(qsim::Matrix<fp_type>& matrix,
                          std::vector<std::string>& axes) {
 
-    //Handle trivial case. Global phase is not observed by norm is adjusted.
+    //Handle trivial case - matrix acting on zero qubits. Global phase is not
+    // observed but norm is adjusted.
     if (matrix.size() == 2) {
       const auto& x = matrix.at(0);
       const auto& y = matrix.at(1);
