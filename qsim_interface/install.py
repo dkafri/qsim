de1fc7e1cf0a532ce7b3576b4b410383642bc270
dkafri <dkafri@google.com>
2021-05-16 20:51:38 -0700

    Clean up install implementation. Allow user to specify python version.

---
+++ qsim_interface/install.py
@@ -0,0 +1,67 @@
+"""Python script that invokes setup.py with optional arguments.
+
+Note: If you are installing from source, you must also pull the pybind
+submodule before running setup.py. Within the qsim_interface directory, do:
+git submodule update --init --recursive
+
+Standard install (from source):
+python install.py
+
+For options, do
+python install.py -h
+
+"""
+import argparse
+import os
+import re
+from typing import Optional
+from warnings import warn
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--python-version', type=str, default=None,
+                    help="Which python version to compile the library with. If "
+                         "not specified CMake will likely use the most recent"
+                         " version.")
+parser.add_argument('--debug', help='Compile in debug mode. The compiled code '
+                                    'evaluates assertions, which can be useful '
+                                    'for debugging, but comes with a minor '
+                                    'performance cost.',
+                    action='store_true')
+
+CONFIG_FILE = 'setup.cfg'
+
+
+def main(debug: bool, python_version: Optional[str]):
+    """Install the qsim_kraus_sim package.
+
+    To see documentation, do: `python install.py -h`
+
+    """
+
+    # confirm python_version is correct format
+    if python_version is not None:
+        expected_format = '3\.([6-9]|1[0-9])(\.[0-9]+)*'
+        match = re.search(expected_format, python_version)
+        if not match or match.end() - match.start() != len(python_version):
+            raise ValueError(f'Require a python version at least 3.6, e.g. '
+                             f'"3.7.2"')
+
+    # We create a temporary setup.cfg with the desired options, then run
+    # setup.py install.
+    if os.path.exists(CONFIG_FILE):
+        warn(f'Config file already exists at {CONFIG_FILE}. This command will'
+             f' now overwrite it.')
+        os.remove(CONFIG_FILE)
+
+    with open(CONFIG_FILE, 'w') as config_file:
+        config_file.write('[build_ext]\n')
+        config_file.write(f'debug={int(debug)}\n')
+        if python_version is not None:
+            config_file.write(f'python_version={python_version}')
+
+    os.system('python setup.py install')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args.debug, args.python_version)
--- qsim_interface/setup.py
+++ qsim_interface/setup.py
@@ -1,16 +1,7 @@
 """Script to compile and install the qsim_kraus_sim pybind interface.
 
-Standard usage:
-python setup.py install
-
-Note: If you are installing from source, you must also pull the pybind
-submodule before running setup.py. Within the qsim_interface directory, do:
-git submodule update --init --recursive
-
-To build a "debug" version, do
-python setup.py --debug install
-this will compile the C++ code with assertions on, which will hurt performance
-but may help with catching bugs.
+This script is used by install.py. You should use that script to install
+the backend.
 
 """
 
@@ -25,7 +16,13 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-__version__ = str(datetime.now().strftime('%y.%m.%d.%H.%M'))
+# specify version by git tag and date
+
+__version__ = str(datetime.now().strftime('%y.%m.%d'))
+
+git_tag = subprocess.check_output(['git', 'rev-parse', 'HEAD'])[:-1].decode(
+    'utf-8')
+__version__ += f'.{git_tag}'
 
 description = 'qsim backend interface for kraus_sim simulator'
 
@@ -35,76 +32,72 @@ requirements = [r.strip().split('#')[0] for r in requirements]
 
 
 class CMakeExtension(Extension):
-  def __init__(self, name, sourcedir=''):
-    Extension.__init__(self, name, sources=[])
-    self.sourcedir = os.path.abspath(sourcedir)
-
-
-# I do not know of a simpler way of passing the debug command to setup.py.
-# Presumably this is not necessary but I don't know how to directly specify
-# the debug attribute.
-DEBUG = False
-if "--debug" in sys.argv:
-  DEBUG = True
-  sys.argv.remove("--debug")
-  __version__ += '.debug'
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
 
 class CMakeBuild(build_ext):
 
-  def __init__(self, dist):
-    super().__init__(dist)
-    self.debug = DEBUG
+    def __init__(self, dist):
+        super().__init__(dist)
+        # These options are specified by the user in a setup.cfg file. See examples
+        # here: https://docs.python.org/3/distutils/configfile.html
+        self.debug = None
+        self.python_version = None
 
-  def run(self):
-    try:
-      out = subprocess.check_output(['cmake', '--version'])
-    except OSError:
-      raise RuntimeError(
-          "CMake must be installed to build the following extensions: " +
-          ", ".join(e.name for e in self.extensions))
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
 
-    if platform.system() == "Windows":
-      cmake_version = LooseVersion(
-          re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-      if cmake_version < '3.1.0':
-        raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(
+                re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
-    for ext in self.extensions:
-      self.build_extension(ext)
+        for ext in self.extensions:
+            self.build_extension(ext)
 
-  def build_extension(self, ext):
-    extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-    # required for auto-detection of auxiliary "native" libs
-    if not extdir.endswith(os.path.sep):
-      extdir += os.path.sep
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
 
-    cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                  '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir]
+        if self.python_version is not None:
+            cmake_args.append(f'-DPYTHON_VERSION={self.python_version}')
 
-    cfg = 'Debug' if self.debug else 'Release'
-    build_args = ['--config', cfg]
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
 
-    if platform.system() == "Windows":
-      cmake_args += [
-          '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-      if sys.maxsize > 2 ** 32:
-        cmake_args += ['-A', 'x64']
-      build_args += ['--', '/m']
-    else:
-      cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-      build_args += ['--', '-j2']
+        if platform.system() == "Windows":
+            cmake_args += [
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(),
+                                                                extdir)]
+            if sys.maxsize > 2 ** 32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
 
-    env = os.environ.copy()
-    env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-        env.get('CXXFLAGS', ''),
-        self.distribution.get_version())
-    if not os.path.exists(self.build_temp):
-      os.makedirs(self.build_temp)
-    subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                          cwd=self.build_temp, env=env)
-    subprocess.check_call(['cmake', '--build', '.'] + build_args,
-                          cwd=self.build_temp)
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''),
+            self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
 
 
 setup(
